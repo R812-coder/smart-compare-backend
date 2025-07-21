3ef89f50a791bc248b8207f61911681e60878954
R812-coder <itsrayan2005@gmail.com>
2025-07-21 19:48:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require("openai");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
 });
+
 const openai = new OpenAIApi(configuration);
 
 app.post("/ask", async (req, res) => {
